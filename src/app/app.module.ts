ed45906176bf394ed888a863b68dbc9c71dd9398
Sebastian Gug <sebastian.gug@ishango.co.uk>
2019-06-22 14:50:38 +0100

    Seed commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PortfolioItemComponent } from './portfolio-item/portfolio-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostComponent,
+    PortfolioComponent,
+    PortfolioItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
